1f58374831112a24eb349270c4f0af1d195028ad
varunreddy04022000 <varunreddy04022000@gmail.com>
2021-11-12 11:23:59 +0530

    MAIN commit

--- ASSIGNMENT 1/2021201026/q5.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-mkdir temp_activity
-cd temp_activity
-touch temp{1..50}.txt
-ls temp{1..25}.txt | sed 'p;s/\.txt/\.md/' | xargs -n2 mv
-ls temp{1..50}.* | sed 'p;s/\./\\_modified./' | xargs -n2 mv
-zip txt_compressed.zip *.txt
--- ASSIGNMENT 2/2021201026_PARTA/README.md
+++
@@ -1,2 +0,0 @@
-# Assignment 2  
-- GITHUB URL: https://varunreddy624.github.io/
