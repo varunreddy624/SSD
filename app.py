76ddd7af944af52bbd60f04e12d0202bc008959b
varunreddy04022000 <varunreddy04022000@gmail.com>
2021-11-18 12:51:31 +0530

    test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
+from flask_session import Session
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
--- customer.py
+++ customer.py
@@ -5,28 +5,16 @@ import requests, json
 
 while(1):
     print("select any of the following choices")
-    print("1. Add item")
-    print("2. Fetch the menu")
+    print("1. Fetch the menu")
     print("3. Exit")
     choice = int(input())
 
     if choice==1:
-        print("enter item id")
-        item_id=int(input())
-        print("enter half plate price")
-        half_plate_price=int(input())
-        print("enter full plate price")
-        full_plate_price=int(input())
-        res = requests.post('http://localhost:8000/create',json= {'item_id':item_id,'half_plate_price':half_plate_price,'full_plate_price':full_plate_price}).content
-        obj = json.loads(res)
-        json_formatted_str = json.dumps(obj, indent=4, separators=(',',': '))
-        print(json_formatted_str)
-
-    elif choice==2:
         res = requests.get('http://localhost:8000/read').content
         obj = json.loads(res)
-        json_formatted_str = json.dumps(obj, indent=4)
-        print(json_formatted_str)
+        print("item id\t\thalf plate price\tfull plate price")
+        for i in obj:
+            print(f'{i}\t\t{obj[i]["half_plate_price"]}\t\t\t{obj[i]["full_plate_price"]}')
         
     elif choice==3:
         break
